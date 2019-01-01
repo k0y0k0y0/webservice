976f62cc1a23aec8596cd567d68c0ac8e2f53c86
taak <papipupepo_10@yahoo.co.jp>
2019-01-01 21:38:11 +0900

    Create page style

--- withdrow.php
+++ withdrow.php
@@ -1,4 +1,24 @@
 <?php
+
+//共通変数・関数ファイルを読込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　WITHDROW.PHP　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//Login authentication
+require('auth.php');
+
+//GET USER DATA FROM DATABASE
+$dbFromData = getUser($_SESSION['user_id']);
+debug('取得したユーザー情報: '.print_r($dbFromData, true));
+
+$userName = $dbFromData['user_name'];
+
+?>
+<?php
 $siteTitle = 'WITHDROW';
 require('./head.php');
 ?>
@@ -16,6 +36,22 @@ require('./head.php');
     <?php
     require('./sideBar.php');
     ?>
+
+    <!-- メイン -->
+    <section id="main" class="with-sidebar">
+      <div class="form-container">
+        <form action="" method="post" class="form">
+          本当に退会しますか？
+          <label>
+            <input type="checkbox" name="withdrow">
+            <span class="checkbox_msg">退会する</span>
+          </label>
+          <div class="btn-container">
+            <input type="submit" class="btn btn-mid" value="WITHDROW">
+          </div>
+        </form>
+      </div>
+    </section>
 
   </div>
 
