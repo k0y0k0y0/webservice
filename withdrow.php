2177f8130b80be69598b4a22601096c7831a6614
taak <papipupepo_10@yahoo.co.jp>
2019-01-01 21:55:49 +0900

    Create logic

--- withdrow.php
+++ withdrow.php
@@ -17,6 +17,32 @@ debug('取得したユーザー情報: '.print_r($dbFromData, true));
 
 $userName = $dbFromData['user_name'];
 
+//post送信されていたら以下の処理を行う
+if(!empty($_POST)){
+  debug('POST送信があります。');
+  try{
+    //DataBase Connection
+    $dbh = dbConnect();
+    //Create SQL
+    $sql1 = 'UPDATE users SET delete_flg = 1 WHERE user_id = :u_id';
+    $data = array('u_id'=>$_SESSION['user_id']);
+    //Query
+    $stmt1 = queryPost($dbh, $sql1, $data);
+    if($stmt1){
+      session_destroy();
+      debug('セッションの中身: '.print_r($_SESSION, true));
+      debug('トップページへ遷移します。');
+      header('Location:index.php');
+    }else{
+      debug('クエリが失敗しました。');
+      $err_msg['common'] = MSG07;
+    }
+  }catch(Exception $e){
+  debug('クエリが失敗しました。'.$e->getMessage());
+  $err_msg['common'] = MSG07;
+  }
+}
+
 ?>
 <?php
 $siteTitle = 'WITHDROW';
@@ -47,7 +73,7 @@ require('./head.php');
             <span class="checkbox_msg">退会する</span>
           </label>
           <div class="btn-container">
-            <input type="submit" class="btn btn-mid" value="WITHDROW">
+            <input type="submit" class="btn btn-mid" value="WITHDROW" name="withdrow">
           </div>
         </form>
       </div>
