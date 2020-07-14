cf73ff9ee8f0aef1f12028fec4b951afda6b78bf
taak <papipupepo_10@yahoo.co.jp>
2020-07-14 23:58:54 +0900

    create errormessages.php

---
+++ errorMesages.php
@@ -0,0 +1,10 @@
+<?php
+//================================
+// ERROE MESSAGE
+//================================
+//prepare array for error message
+$err_msg = array();
+
+//error messages
+define('MSG01', '入力必須です');
+?>
