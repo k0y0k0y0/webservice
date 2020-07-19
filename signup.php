79bc977b18503a6c6a6e508c36ae04805b94b73c
taak <papipupepo_10@yahoo.co.jp>
2020-07-19 15:34:03 +0900

    add php files

--- signup.php
+++ signup.php
@@ -2,9 +2,26 @@
 
 //共通変数・関数ファイルを読込み
 require('./function.php');
+require('./errorMessages.php');
 
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debug('「　SIGNUP.PHP　');
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debugLogStart();
+
+//post送信されていたら以下の処理を行う
+if(!empty($_POST)){
+
+}
 ?>
+<?php
+$siteTitle = 'SIGNUP';
+require('./head.php');
+?>
+<body>
+
+  <!-- ヘッダー -->
+  <?php
+  require('./header.php');
+  ?>
+</body>
---
+++ validations.php
@@ -0,0 +1,56 @@
+<?php
+//=====================
+//　FUNCTIONS OF VALIDATION
+//=====================
+//バリデーション関数（未入力チェック）
+function validRequired($str, $key){
+  if($str === ''){ //金額フォームなどを考えると数値の０はOKにし、空文字はダメにする
+    global $err_msg;
+    $err_msg[$key] = MSG01;
+  }
+}
+//バリデーション関数（Email形式チェック）
+function validEmail($str, $key){
+  if(!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $str)){
+    global $err_msg;
+    $err_msg[$key] = MSG02;
+  }
+}
+//バリデーション関数（同値チェック）
+function validMatch($str1, $str2, $key){
+  if($str1 !== $str2){
+    global $err_msg;
+    $err_msg[$key] = MSG03;
+  }
+}
+//バリデーション関数（最小文字数チェック）
+function validMinLen($str, $key, $min = 6){
+  if(mb_strlen($str) < $min){
+    global $err_msg;
+    $err_msg[$key] = MSG05;
+  }
+}
+//バリデーション関数（最大文字数チェック）
+function validMaxLen($str, $key, $max = 256){
+  if(mb_strlen($str) > $max){
+    global $err_msg;
+    $err_msg[$key] = MSG06;
+  }
+}
+//バリデーション関数（半角チェック）
+function validHalf($str, $key){
+  if(!preg_match("/^[a-zA-Z0-9]+$/", $str)){
+    global $err_msg;
+    $err_msg[$key] = MSG04;
+  }
+}
+//電話番号形式チェック
+function validTel($str, $key){
+  if(!preg_match("/0\d{1,4}\d{1,4}\d{4}/", $str)){
+    global $err_msg;
+    $err_msg[$key] = MSG10;
+  }
+}
+
+
+?>
