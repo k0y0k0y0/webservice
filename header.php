67cec5f2c13a6adf9ce604c06b4b09f59b63b56e
taak <papipupepo_10@yahoo.co.jp>
2020-07-23 13:59:31 +0900

    Cleate header.php

--- header.php
+++ header.php
@@ -1,4 +1,40 @@
 <header>
-  <div>
+  <div class="site-width">
+    <nav id='top-nav-left'>
+      <ul>
+        <?php
+          if(empty($_SESSION['user_id'])){
+          ?>
+            <li><a href="./signup.php">SIGNUP</a></li>
+            <li><a href="./login.php">LOGIN</a></li>
+          <?
+          }else{
+          ?>
+            <li><a href="./mypage.php">MYPAGE</a></li>
+
+            <li><a href="./login.php">LOGIN</a></li>
+          <?php
+          }
+          ?>
+      </ul>
+    </nav>
+    <h1><a href="./index.php">PHOTOTO</a></h1>
+    <nav id='top-nav-right'>
+      <ul>
+        <?php
+        if(empty($_SESSION['user_id'])){
+        ?>
+          <li><a href="./signup.php">SIGNUP</a></li>
+          <li><a href="./login.php">LOGIN</a></li>
+        <?
+        }else{
+        ?>
+          <li><a href="./mypage.php">MYPAGE</a></li>
+          <li><a href="./login.php">LOGIN</a></li>
+        <?php
+        }
+        ?>
+      </ul>
+    </nav>
   </div>
 </header>
