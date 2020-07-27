106ff7fc4113dde65bb57e3f224fa14a73a134f3
taak <papipupepo_10@yahoo.co.jp>
2020-07-27 20:47:36 +0900

    Modify

--- signup.php
+++ signup.php
@@ -3,6 +3,7 @@
 //共通変数・関数ファイルを読込み
 require('./function.php');
 require('./errorMesages.php');
+require('./validations.php');
 
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debug('「　SIGNUP.PHP　');
@@ -12,67 +13,73 @@ debugLogStart();
 //post送信されていたら以下の処理を行う
 if(!empty($_POST)){
   //POSTの中身を変数に代入
+  $name = $_POST['name'];
   $email = $_POST['email'];
   $pass = $_POST['pass'];
   $pass_re = $_POST['pass_re'];
 
   //Validation Check
   //1.未入力
-  validRequired($eamil, 'email');
+  validRequired($name, 'name');
+  validRequired($email, 'email');
   validRequired($pass, 'pass');
-  validRequired($pass_re, 'pass_er');
+  validRequired($pass_re, 'pass_re');
 
   //Next Step or Not
   if(empty($err_msg)){
-    //2.email
-    validEmail($eamil, 'email');
-    validMaxLen($eamil, 'email');
-    validEmailDup($email);
-
-    //Next Step or Not
+    //2.name
+    validMaxLen($name, 'name');
     if(empty($err_msg)){
-      //3.password
-      validHalf($pass, 'pass');
-      validMaxLen($pass, 'pass');
-      validMinLen($pass, 'pass');
+      //3.email
+      validEmail($email, 'email');
+      validMaxLen($email, 'email');
+      validEmailDup($email);
 
       //Next Step or Not
       if(empty($err_msg)){
-        //4.password re
-        validMatch($pass, $pass_re, 'pass_re');
+        //4.password
+        validHalf($pass, 'pass');
+        validMaxLen($pass, 'pass');
+        validMinLen($pass, 'pass');
 
+        //Next Step or Not
         if(empty($err_msg)){
-          try{
-            //Data Base Connection
-            $dbh = dbConnect();
-            //Create SQL
-            $sql = 'INSERT INTO user (name, email, password, login_time, create_date)
-                    VALUES (:name, :email, :pass, :login_time, :create_date)';
-            $data = array(':name' => $name,
-                          ':email' => $email,
-                          ':pass' => password_hash($pass, PASSWORD_DEFAULT),
-                          ':login_time' => date('Y-m-d H:i:s'),
-                          ':create_date' => date('Y-m-d H:i:s'));
-            //Query
-            $stmt = queryPost($dbh, $sql, $data);
-            //クエリ成功の場合
-            if($stmt){
-              //ログイン有効期限(default=1[h])
-              $seeLimit = 60*60;
-              //最終ログインを現在日時に
-              $_SESSION['login_data'] = time();
-              $_SESSION['login_limit'] = $seeLimit;
-              //ユーザーIDを格納
-              $_SESSION['user_id'] = $dbh->lastInsertId();
+          //5.password re
+          validMatch($pass, $pass_re, 'pass_re');
 
-              debug('セッション変数の中身: '.print_r($_SESSION, true));
+          if(empty($err_msg)){
+            try{
+              //Data Base Connection
+              $dbh = dbConnect();
+              //Create SQL
+              $sql = 'INSERT INTO user (username, email, password, login_time, create_date)
+                      VALUES (:name, :email, :pass, :login_time, :create_date)';
+              $data = array(':name' => $name,
+                            ':email' => $email,
+                            ':pass' => password_hash($pass, PASSWORD_DEFAULT),
+                            ':login_time' => date('Y-m-d H:i:s'),
+                            ':create_date' => date('Y-m-d H:i:s'));
+              //Query
+              $stmt = queryPost($dbh, $sql, $data);
+              //クエリ成功の場合
+              if($stmt){
+                //ログイン有効期限(default=1[h])
+                $seeLimit = 60*60;
+                //最終ログインを現在日時に
+                $_SESSION['login_data'] = time();
+                $_SESSION['login_limit'] = $seeLimit;
+                //ユーザーIDを格納
+                $_SESSION['user_id'] = $dbh->lastInsertId();
 
-              //Go to mypage
-              header('Location:mypage.php');
+                debug('セッション変数の中身: '.print_r($_SESSION, true));
+
+                //Go to mypage
+                header('Location:mypage.php');
+              }
+            }catch(Exception $e){
+              error_log('エラー発生: '.$e->getMessage());
+              $err_msg['common'] = MSG07;
             }
-          }catch(Exception $e){
-            error_log('エラー発生: '.$e->getMessage());
-            $err_msg['common'] = MSG07;
           }
         }
       }
