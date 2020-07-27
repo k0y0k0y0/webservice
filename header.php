d81d9b703fec56c9de359758f109634dd0bf8bfe
taak <papipupepo_10@yahoo.co.jp>
2020-07-27 21:55:37 +0900

    modify header.php

--- header.php
+++ header.php
@@ -3,19 +3,18 @@
     <nav id='top-nav-left'>
       <ul>
         <?php
-          if(empty($_SESSION['user_id'])){
-          ?>
-            <li><a href="./signup.php">SIGNUP</a></li>
-            <li><a href="./login.php">LOGIN</a></li>
-          <?
-          }else{
-          ?>
-            <li><a href="./mypage.php">MYPAGE</a></li>
-
-            <li><a href="./login.php">LOGIN</a></li>
-          <?php
-          }
-          ?>
+        if(empty($_SESSION['user_id'])){
+        ?>
+          <li><a href="./signup.php">SIGNUP</a></li>
+          <li><a href="./login.php">LOGIN</a></li>
+        <?php
+        }else{
+        ?>
+          <li><a href="./mypage.php">MYPAGE</a></li>
+          <li><a href="./logout.php">LOGOUT</a></li>
+        <?php
+        }
+        ?>
       </ul>
     </nav>
     <h1><a href="./index.php">PHOTOTO</a></h1>
@@ -26,11 +25,11 @@
         ?>
           <li><a href="./signup.php">SIGNUP</a></li>
           <li><a href="./login.php">LOGIN</a></li>
-        <?
+        <?php
         }else{
         ?>
           <li><a href="./mypage.php">MYPAGE</a></li>
-          <li><a href="./login.php">LOGIN</a></li>
+          <li><a href="./logout.php">LOGOUT</a></li>
         <?php
         }
         ?>
