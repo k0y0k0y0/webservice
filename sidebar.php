de4d03d4793a7239adde2da94dc93a468d1f4145
taak <papipupepo_10@yahoo.co.jp>
2020-08-01 12:11:26 +0900

    Add sidebar navigation

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,75 @@
-<section id="isdebar">
-  
+<section id="sidebar">
+  <div class="prof-img">
+    <img src="./img/profile/noimage.jpg">
+  </div>
+  <h3><?php echo $userName;?></h3>
+  <nav id="sidebar-nav">
+    <ul>
+      <li>
+        <?php
+        if(basename($_SERVER['PHP_SELF']) === 'post.php'){
+        ?>
+          POST
+        <?php
+        }else{
+        ?>
+          <a href="./post.php">POST</a>
+        <?php
+        }
+        ?>
+      </li>
+      <li>
+        <?php
+        if(basename($_SERVER['PHP_SELF']) === 'profEdit.php'){
+        ?>
+          PROFILE EDIT
+        <?php
+        }else{
+        ?>
+          <a href="./profEdit.php">PROFILE EDIT</a>
+        <?php
+        }
+        ?>
+      </li>
+      <li>
+        <?php
+        if(basename($_SERVER['PHP_SELF']) === 'passEdit.php'){
+        ?>
+          PASSWORD EDIT
+        <?php
+        }else{
+        ?>
+          <a href="./passEdit.php">PASSWORD EDIT</a>
+        <?php
+        }
+        ?>
+      </li>
+      <li>
+        <?php
+        if(basename($_SERVER['PHP_SELF']) === 'post.php'){
+        ?>
+          DIRECT MESSAGE
+        <?php
+        }else{
+        ?>
+          <a href="./post.php">DIRECT MESSAGE</a>
+        <?php
+        }
+        ?>
+      </li>
+      <li>
+        <?php
+        if(basename($_SERVER['PHP_SELF']) === 'withdrow.php'){
+        ?>
+          WITHDROW
+        <?php
+        }else{
+        ?>
+          <a href="./withdrow.php">WITHDROW</a>
+        <?php
+        }
+        ?>
+      </li>
+    </ul>
+  </nav>
 </section>
