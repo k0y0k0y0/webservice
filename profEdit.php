3611ac0e11cb3860fedb2a1865e35202b0f30127
taak <papipupepo_10@yahoo.co.jp>
2020-08-05 22:00:54 +0900

    Create logic in profedit.php

--- profEdit.php
+++ profEdit.php
@@ -2,6 +2,8 @@
 
 //共通変数・関数ファイルを読込み
 require('function.php');
+require('./errorMesages.php');
+require('./validations.php');
 
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debug('「　PROFEDIT.PHP　');
@@ -15,8 +17,76 @@ require('auth.php');
 $dbFromData = getUser($_SESSION['user_id']);
 debug('取得したユーザー情報: '.print_r($dbFromData, true));
 
-$userName = $dbFromData['user_name'];
+//POST送信があった場合
+if(!empty($_POST)){
+  debug('POST送信があります。');
+  debug('POST情報: '.print_r($_POST, true));
 
+  //変数にユーザー情報を代入
+  $user_name = $_POST['user_name'];
+  $email = $_POST['email'];
+  $job = $_POST['job'];
+  $area = $_POST['area'];
+  $comment = $_POST['comment'];
+
+  //DBの情報と入力情報が異なる場合のみValidationCheckを行う
+  if($dbFromData['user_name'] !== $user_name){
+    //名前の最大文字数チェック
+    validMaxLen($user_name, 'user_name');
+  }
+  if($dbFromData['email'] !== $email){
+    //emailの最大文字数チェック
+    validMaxLen($email, 'email');
+    if(empty($err_msg['email'])){
+      //emailの重複チェック
+      validEmailDup($email);
+    }
+    //emailの形式チェック
+    validEmail($email, 'email');
+    //emailの未入力チェック
+    validRequired($email, 'email');
+  }
+  if($dbFromData['job'] !== $job){
+    validMaxLen($job, 'job');
+  }
+  if($dbFromData['area'] !== $area){
+    validMaxLen($area, 'area');
+  }
+  if($dbFromData['comment'] !== $comment){
+    validMaxLen($comment, 'comment');
+  }
+
+  if(empty($err_msg)){
+    debug('Validation OKです！');
+    try{
+      //DataBase Connection
+      $dbh = dbConnect();
+      //Create SQL
+      $sql = 'UPDATE users SET user_name = :user_name, email = :email, job = :job, area = :area, comment = :comment WHERE user_id = :user_id';
+      $data = array(':user_name' => $user_name, ':email' => $email, ':job' => $job, ':area' => $area, ':comment' => $comment, ':user_id' => $dbFromData['user_id']);
+      //Query
+      $stmt = queryPost($dbh, $sql, $data);
+
+      //クエリ成功の場合
+      if($stmt){
+        $_SESSION['msg_success'] = SUC02;
+        //ここを変更する必要がある
+        //値の保存後にどういう処理を行うのか？
+        debug('マイページへ遷移します。');
+        header("Location:profEdit.php"); //マイページへ
+        //ここまで
+      }else{
+        debug('クエリが失敗しました。');
+        $err_msg['common'] = MSG07;
+      }
+    }catch(Exception $e){
+      error_log('エラー発生'.$e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+}
+
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 ?>
 <?php
 $siteTitle = 'PROFEDIT';
@@ -34,6 +104,7 @@ require('./head.php');
 
     <!-- サイドバー -->
     <?php
+    $userName = $dbFromData['user_name'];
     require('./sideBar.php');
     ?>
 
@@ -41,14 +112,14 @@ require('./head.php');
     <section id="main" class="with-sidebar">
       <div class="form-container">
         <form action="" method="post" class="form">
-          <label class="<?php if(!empty($err_msg['name'])) echo 'err'; ?>">
+          <label class="<?php if(!empty($err_msg['user_name'])) echo 'err'; ?>">
             NAME
             <span class="form_msg">
               <?php
-                if(!empty($err_msg['name'])) echo $err_msg['name'];
+                if(!empty($err_msg['user_name'])) echo $err_msg['user_name'];
               ?>
             </span>
-            <input type="text" name="name" value="<?php if(!empty($_POST['name'])) echo$_POST['name']; ?>">
+            <input type="text" name="user_name" value="<?php echo getFormData('user_name'); ?>">
           </label>
           <label class="<?php if(!empty($err_msg['email'])) echo 'err'; ?>">
             EMAIL
@@ -57,7 +128,7 @@ require('./head.php');
                 if(!empty($err_msg['email'])) echo $err_msg['email'];
               ?>
             </span>
-            <input type="text" name="email" value="<?php if(!empty($_POST['email'])) echo$_POST['email']; ?>">
+            <input type="text" name="email" value="<?php echo getFormData('email'); ?>">
           </label>
           <label class="<?php if(!empty($err_msg['job'])) echo 'err'; ?>">
             JOB
@@ -66,7 +137,7 @@ require('./head.php');
                 if(!empty($err_msg['job'])) echo $err_msg['job'];
               ?>
             </span>
-            <input type="text" name="job" value="<?php if(!empty($_POST['job'])) echo$_POST['job']; ?>">
+            <input type="text" name="job" value="<?php echo getFormData('job'); ?>">
           </label>
           <label class="<?php if(!empty($err_msg['area'])) echo 'err'; ?>">
             AREA
@@ -75,7 +146,7 @@ require('./head.php');
                 if(!empty($err_msg['area'])) echo $err_msg['area'];
               ?>
             </span>
-            <input type="text" name="area" value="<?php if(!empty($_POST['area'])) echo$_POST['area']; ?>">
+            <input type="text" name="area" value="<?php echo getFormData('area'); ?>">
           </label>
           <label class="<?php if(!empty($err_msg['comment'])) echo 'err'; ?>">
             COMMENT
@@ -84,7 +155,7 @@ require('./head.php');
                 if(!empty($err_msg['comment'])) echo $err_msg['comment'];
               ?>
             </span>
-            <textarea name="comment"></textarea>
+            <textarea name="comment"><?php echo getFormData('comment'); ?></textarea>
           </label>
           <div class="btn-container">
             <input type="submit" class="btn btn-mid" value="SAVE">
