2378ff8bd42f1c572f43600892733785e088452d
taak <papipupepo_10@yahoo.co.jp>
2020-08-09 11:45:32 +0900

    Add uploadImg

--- function.php
+++ function.php
@@ -121,8 +121,60 @@ function getFormData($str, $flg = false){
 }
 
 
-// サニタイズ
+//サニタイズ
 function sanitize($str){
   return htmlspecialchars($str,ENT_QUOTES);
+}
+
+
+//画像処理
+function uploadImg($file, $key){
+  debug('画像アップロード処理開始');
+  debug('FILE情報: '.print_r($file, true));
+
+  // $file['error'] の値を確認。配列内には「UPLOAD_ERR_OK」などの定数が入っている。
+  if(isset($file['error']) && is_int($file['error'])){
+    //「UPLOAD_ERR_OK」などの定数はphpでファイルアップロード時に自動的に定義される。定数には値として0や1などの数値が入っている。
+    try{
+      switch($file['error']){
+        case UPLOAD_ERR_OK:
+          break;
+        case UPLOAD_ERR_NO_FILE:
+          throw new RuntimeException('ファイルが選択されていません。');
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+          throw new RuntimeException('ファイルサイズが大きすぎます。');
+        default:
+          throw new RuntimeException('その他のエラーが発生しました。');
+      }
+
+      // $file['mime']の値はブラウザ側で偽装可能なので、MIMEタイプを自前でチェックする
+      // exif_imagetype関数は「IMAGETYPE_GIF」「IMAGETYPE_JPEG」などの定数を返す
+      $type = @exif_imagetype($file['tmp_name']);
+      if (!in_array($type, [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) { // 第三引数にはtrueを設定すると厳密にチェックしてくれるので必ずつける
+          throw new RuntimeException('画像形式が未対応です');
+      }
+
+      // ファイルデータからSHA-1ハッシュを取ってファイル名を決定し、ファイルを保存する
+      // ハッシュ化しておかないとアップロードされたファイル名そのままで保存してしまうと同じファイル名がアップロードされる可能性があり、
+      // DBにパスを保存した場合、どっちの画像のパスなのか判断つかなくなってしまう
+      // image_type_to_extension関数はファイルの拡張子を取得するもの
+      $path = 'uploads/'.sha1_file($file['tmp_name']).image_type_to_extension($type);
+      if (!move_uploaded_file($file['tmp_name'], $path)) { //ファイルを移動する
+          throw new RuntimeException('ファイル保存時にエラーが発生しました');
+      }
+      // 保存したファイルパスのパーミッション（権限）を変更する
+      chmod($path, 0644);
+
+      debug('ファイルは正常にアップロードされました');
+      debug('ファイルパス：'.$path);
+      return $path;
+
+    } catch (RuntimeException $e) {
+      debug($e->getMessage());
+      global $err_msg;
+      $err_msg[$key] = $e->getMessage();
+    }
+  }
 }
 ?>
