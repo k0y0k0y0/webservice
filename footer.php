113de84b58a8ca8777b321c4588b1ab2ec11e972
taak <papipupepo_10@yahoo.co.jp>
2020-08-28 16:58:17 +0900

    add jquery CDN

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
 <footer id="footer">
   Copyright <a href="#">PHOTOTO</a> All RIGHTS Reserved.
 </footer>
-<!--  -->
-<script></script>
+
+<!-- script -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="./main.js"></script>
 </body>
 </html>
