faf0aa08a158b5d7c14e9d54e8393666d2806107
taak <papipupepo_10@yahoo.co.jp>
2020-09-02 22:27:29 +0900

    Add javascript file

---
+++ main.js
@@ -0,0 +1,56 @@
+$(function(){
+  //footer
+  var $ftr = $('#footer');
+  if(window.innerHeight > $ftr.offset().top + $ftr.outerHeight()){
+    $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftrouterHeight()) + 'px;'});
+  }
+
+  //message
+  var $jsShowMsg = $('#js-show-msg');
+  var msg = $jsShowMsg.text();
+  if(msg.replace(/^[\s　]+|[\s　]+$/g, "").length){
+    $jsShowMsg.slideToggle('slow');
+    setTimeout(function(){ $jsShowMsg.slideToggle('slow'); }, 5000);
+  }
+
+  //image library review
+  var $dropArea = $('.area-drop');
+  var $fileInput = $('.input-file');
+  $dropArea.on('dragover', function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).css('border', '3px #ccc dashed');
+  });
+  $dropArea.on('dragleave', function(e){
+    e.stopPropagation();
+    e.preventDefault();
+    $(this).css('border', 'none');
+  });
+  $fileInput.on('change', function(e){
+    $dropArea.css('border', 'none');
+    var file = this.files[0],            // 2.files配列にファイルが入っています
+        $img = $(this).siblings('.prev-img'), // 3.jQueryのsiblingsメソッドで兄弟のimgを取得
+        fileReader = new FileReader();   // 4.ファイルを読み込むFileReaderオブジェクト
+    // 5.読み込みが完了した際のイベントハンドラ。imgのsrcにデータをセット
+    fileReader.onload = function(event) {
+      // 読み込んだデータをimgに設定
+      $img.attr('src', event.target.result).show();
+    };
+    // 6.画像読み込み
+    fileReader.readAsDataURL(file);
+  });
+
+  //text area count
+  var $countUp = $('#js-count'),
+      $countView = $('#js-count-view');
+  $countUp.on('keyup', function(e){
+    $countView.html($(this).val().length);
+  });
+
+  //image change
+  var $switchImgSubs = $('.js-switch-img-sub'),
+      $switchImgMain = $('#js-switch-img-main');
+  $switchImgSubs.on('click',function(e){
+    $switchImgMain.attr('src',$(this).attr('src'));
+  });
+});
